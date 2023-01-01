91f02efd65fd0031e03a21d581e13c68acb2a18e
caperaven <caperaven@gmail.com>
2023-01-01 12:40:44 +0200

    refactor init

--- old/crs-loader.js
+++
@@ -1,7 +0,0 @@
-export class Loader extends HTMLElement {
-    connectedCallback() {
-        fetch(import.meta.url.replace(".js", ".html")).then(result => result.text()).then(html => this.innerHTML = html);
-    }
-}
-
-customElements.define("crs-loader", Loader);
